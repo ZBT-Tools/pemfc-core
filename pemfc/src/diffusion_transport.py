effective transport calculation not correct yet, diffusion solver flux not correct

pemfc/src/diffusion_transport.py
@@ -34,6 +34,8 @@ class DiffusionTransport(ABC):
             ls.BasicLinearSystem.create(item, self.transport_type,
                                         previously_shifted_axis=item.shift_axis)
             for item in self.transport_layers]
+        # for lin_sys in self.linear_systems:
+        #     lin_sys.sparse_solve = False
         self.base_shape = self.linear_systems[0].solution_array.shape
         if len(self.linear_systems) == 1:
             solution_shape = self.base_shape
@@ -70,7 +72,7 @@ class DiffusionTransport(ABC):
                 raise ValueError('for type GasMixtureDiffusionTransport the '
                                  'integer value "id_inert" must be specified')
             input_dict['volume_fraction'] = input_dict['porosity']
-            input_dict['effective'] = True
+            input_dict['effective'] = False
             species_names = fluid.species_names
             transport_layers = [
                 tl.TransportLayer.create(
@@ -364,7 +366,9 @@ class GasMixtureDiffusionTransport(DiffusionTransport):
         for i, trans_layer in enumerate(self.transport_layers):
             trans_layer.update(
                 {self.transport_type:
-                    self.gas_diff_coeff.d_eff[self.ids_active[i]]},
+                    [self.gas_diff_coeff.d_eff[self.ids_active[i]],
+                     self.gas_diff_coeff.d_eff[self.ids_active[i]] * 0.0,
+                     self.gas_diff_coeff.d_eff[self.ids_active[i]] * 0.0]},
                 volume_fraction=volume_fraction)
             # trans_layer.conductance['diffusion'][:, 0:5, :, 0:10] = 1e-16
 
@@ -405,15 +409,15 @@ class GasMixtureDiffusionTransport(DiffusionTransport):
         self.solution_array = concentrations
         show_concentrations = np.moveaxis(concentrations,
                                           (0, 1, 2, 3), (0, 2, 1, 3))
-        # conductances = [lin_sys.conductance for lin_sys in self.linear_systems]
-        if isinstance(self.fluid, (fl.GasMixture, fl.CanteraGasMixture)):
-            self.fluid.update(temperature, pressure,
-                              mole_composition=self.solution_array)
-        elif isinstance(self.fluid, (fl.TwoPhaseMixture,
-                        fl.CanteraTwoPhaseMixture)):
-            self.fluid.update(temperature, pressure,
-                              gas_mole_composition=self.solution_array)
-
+        conductances = [lin_sys.conductance for lin_sys in self.linear_systems]
+        # if isinstance(self.fluid, (fl.GasMixture, fl.CanteraGasMixture)):
+        #     self.fluid.update(temperature, pressure,
+        #                       mole_composition=self.solution_array)
+        # elif isinstance(self.fluid, (fl.TwoPhaseMixture,
+        #                 fl.CanteraTwoPhaseMixture)):
+        #     self.fluid.update(temperature, pressure,
+        #                       gas_mole_composition=self.solution_array)
+        diff_conc = np.diff(conc_array, axis=1)
         self.initialize = False
 
     def calc_boundary_flux(self, boundary_type: str):

pemfc/src/half_cell.py
@@ -125,7 +125,7 @@ class HalfCell:
                                        self.discretization)
 
         self.calc_gdl_diffusion = True
-        self.calc_two_phase_flow = True
+        self.calc_two_phase_flow = False
 
         if self.calc_gdl_diffusion:
             gdl_diffusion_dict = gde_dict.copy()
@@ -140,15 +140,15 @@ class HalfCell:
             #     electrochemistry_dict['diff_coeff_gdl'])
             if self.channel_land_discretization:
                 # Initialize diffusion transport model
-                nx = 4
+                nx = 2
                 ny = self.discretization.shape[0]
-                nz = 8
+                nz = 2
                 gdl_diffusion_dict['boundary_patches']['Dirichlet'] = {
                     'axes': (0, 2), 'indices': (0, list(range(int(nz/2), nz)))}
             else:
-                nx = 2
+                nx = 4
                 ny = self.discretization.shape[0]
-                nz = 1
+                nz = 8
                 gdl_diffusion_dict['boundary_patches']['Dirichlet'] = {
                     'axes': (0,), 'indices': (0,)}
             discretization_dict = {
@@ -317,11 +317,13 @@ class HalfCell:
 
                 mass_flux_old = np.copy(mass_flux)
                 mole_flux_old = np.copy(mole_flux)
+                mole_flux_avg_cl_old = np.average(
+                    np.average(mole_flux_old, axis=-1), axis=-1)
                 # mole_flux_avg_old = np.average(np.average(mole_flux, axis=-1),
                 #                                axis=-1)
                 # Recalculate mass flux at GDL/Channel interface
                 bc_type = 'Dirichlet'
-                mole_flux = - self.gdl_diffusion.calc_boundary_flux(bc_type)
+                mole_flux = self.gdl_diffusion.calc_boundary_flux(bc_type)
                 mole_flux_avg = np.average(np.average(mole_flux, axis=-1),
                                            axis=-1)
                 mass_flux = (mole_flux.transpose() *
@@ -333,23 +335,27 @@ class HalfCell:
                         self.gdl_diffusion.discretization.d_area[axes[0]],
                         axes,
                         indices))
+                mole_source_chl = np.sum(np.sum(flux_interface_area * mole_flux,
+                                                axis=-1), axis=-1)
+                # Recalculate mass flux at GDL/Channel interface
+                bc_type = 'Neumann'
+                mole_flux_cl = - self.gdl_diffusion.calc_boundary_flux(
+                    bc_type)
+                mole_flux_avg_cl = np.average(np.average(mole_flux_cl, axis=-1),
+                                              axis=-1)
 
-                # # Recalculate mass flux at GDL/Channel interface
-                # bc_type = 'Neumann'
-                # mole_flux_cl = - self.gdl_diffusion.calc_boundary_flux(
-                #     bc_type)
-                # mole_flux_avg_cl = np.average(np.average(mole_flux_cl, axis=-1),
-                #                               axis=-1)
-                #
-                # mass_flux_cl = (mole_flux_cl.transpose() *
-                #                 self.channel.fluid.species_mw).transpose()
-                # axes, indices, _ = self.gdl_diffusion.get_boundary_indices(
-                #     bc_type)
-                # flux_interface_area_cl = (
-                #     self.gdl_diffusion.get_values(
-                #         self.gdl_diffusion.discretization.d_area[axes[0]],
-                #         axes,
-                #         indices))
+                mass_flux_cl = (mole_flux_cl.transpose() *
+                                self.channel.fluid.species_mw).transpose()
+                axes, indices, _ = self.gdl_diffusion.get_boundary_indices(
+                    bc_type)
+                flux_interface_area_cl = (
+                    self.gdl_diffusion.get_values(
+                        self.gdl_diffusion.discretization.d_area[axes[0]],
+                        axes,
+                        indices))
+                mole_source_cl = np.sum(np.sum(mole_flux_cl *
+                                        flux_interface_area_cl,
+                                        axis=-1), axis=-1)
                 # Factor 2 due to symmetry in gdl_diffusion model
                 flux_interface_area *= 2.0
 
@@ -366,25 +372,36 @@ class HalfCell:
             if update_channel:
                 # Calculate mole and mass source from fluxes
                 mass_source = self.surface_flux_to_channel_source(
-                    -mass_flux, area=flux_interface_area)
+                    mass_flux, area=flux_interface_area)
                 # mass_source_old = self.surface_flux_to_channel_source(
                 #     mass_flux_old)
                 mole_source = self.surface_flux_to_channel_source(
-                    -mole_flux, flux_interface_area)
+                    mole_flux, flux_interface_area)
+                mole_source_sum = np.sum(mole_source, axis=-1)
                 self.channel.mass_source[:], self.channel.mole_source[:] = (
                     mass_source, mole_source)
-                self.channel.update(update_mass=True, update_flow=False,
-                                    update_heat=False, update_fluid=True)
+                # Only updating mass and mole sources here, because channel
+                # mass flow is updated in ParallelFlowCircuit calculations
+                # even for single cell, so be careful to modify this here
+                # self.channel.update(update_mass=True, update_flow=False,
+                #                     update_heat=False, update_fluid=True)
             # if gs.global_state.iteration == 50:
             #     print('test')
             self.update_voltage_loss(current_density)
 
             # Calculate stoichiometry
             current = self.surface_flux_to_channel_source(current_density)
+            current_sum = np.sum(current)
+            mole_sum = self.faraday_conversion(current_sum, self.n_stoi[
+                self.id_fuel])
+            stoi_test = mole_source_sum / mole_sum
+            inlet_mole_source = self.channel.mole_flow[self.id_fuel, self.channel.id_in]
             self.inlet_stoi = (
                 self.channel.mole_flow[self.id_fuel, self.channel.id_in]
                 * self.faraday * self.n_charge
                 / (np.sum(current) * abs(self.n_stoi[self.id_fuel])))
+            if gs.global_state.iteration == 50:
+                print('test')
             if current_control and self.inlet_stoi < 1.0:
                 raise ValueError('stoichiometry of cell {0} '
                                  'becomes smaller than one: {1:0.3f}'
@@ -408,6 +425,10 @@ class HalfCell:
                     * inlet_composition[i] / inlet_composition[self.id_fuel]
         mass_flow_in = mole_flow_in * self.channel.fluid.species_mw
         return mass_flow_in, mole_flow_in
+
+    def faraday_conversion(self, electrical_current, stoich_factor):
+        return (electrical_current * abs(stoich_factor) /
+                (self.n_charge * self.faraday))
 
     def calc_species_flux(self, current_density: np.ndarray):
         if not isinstance(current_density, np.ndarray):

pemfc/src/linear_system.py
@@ -7,7 +7,7 @@ import numpy as np
 # from numpy import linalg
 from scipy import linalg as sp_la
 from scipy import sparse
-from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import spsolve, cg, gmres
 from dataclasses import dataclass
 
 # Local module imports
@@ -91,13 +91,20 @@ class LinearSystem(ABC):
         Solves for the solution vector.
         """
         if self.sparse_solve:
-            # cond_number = np.linalg.cond(self.mtx)
-            mtx = sparse.csr_matrix(self.mtx)
-            self.solution_vector[:] = spsolve(mtx, self.rhs)
+            cond_number = np.linalg.cond(self.mtx)
+            mtx = sparse.csr_matrix(self.mtx, dtype=np.longdouble)
+            mtx = np.array(self.mtx, dtype=np.longdouble)
+            precond_mtx = mtx_func.spai(self.mtx, 10)
+            cond_number_precond = np.linalg.cond(self.mtx @
+                                                 precond_mtx)
+            rhs = np.array(self.rhs, dtype=np.longdouble)
+            self.solution_vector[:], _ = cg(mtx, rhs, M=precond_mtx, rtol=1e-12)
         else:
-            self.solution_vector[:] = np.linalg.tensorsolve(self.mtx, self.rhs)
+            cond_number = np.linalg.cond(self.mtx)
+            self.solution_vector[:] = np.linalg.solve(self.mtx, self.rhs)
         self.solution_array[:] = np.reshape(
             self.solution_vector, self.shape, order='F')
+        print('test')
 
     def add_implicit_source(self, matrix, coefficients,
                             index_array=None, replace=False):
@@ -291,39 +298,6 @@ class BasicLinearSystem(LinearSystem, ABC):
         ids_flat = np.arange(self.solution_vector.shape[0])
         return np.reshape(ids_flat, self.solution_array.shape, order='F')
 
-    def update_rhs(self, *args, **kwargs):
-        """
-        Create vector with the right hand side entries,
-        Sources from outside the src to the src must be defined negative.
-        """
-        self.rhs[:] = 0.0
-        rhs_input = kwargs.get('rhs_input', None)
-        if isinstance(rhs_input, RHSInput):
-            if isinstance(rhs_input.neumann_bc, BoundaryData):
-                self.set_neumann_boundary_conditions(
-                    rhs_input.neumann_bc.values,
-                    rhs_input.neumann_bc.axes,
-                    rhs_input.neumann_bc.indices)
-            if isinstance(rhs_input.dirichlet_bc, BoundaryData):
-                self.set_dirichlet_boundary_conditions(
-                    rhs_input.dirichlet_bc.values,
-                    rhs_input.dirichlet_bc.axes,
-                    rhs_input.dirichlet_bc.indices)
-            if isinstance(rhs_input.explicit_sources, SourceData):
-                sources = rhs_input.explicit_sources.values
-                source_vector = sources.ravel(order='F')
-                if rhs_input.dirichlet_bc is not None:
-                    index_vector_remaining = self.get_remaining_indices(
-                        rhs_input.dirichlet_bc.axes,
-                        rhs_input.dirichlet_bc.indices)
-                    source_vector[index_vector_remaining] = 0.0
-                self.add_source_data(
-                    source_vector,
-                    rhs_input.explicit_sources.indices,
-                    rhs_input.explicit_sources.volumetric
-                )
-        # self.rhs[:] = self.rhs_const + self.rhs_dyn
-
     def update_matrix(self, *args, **kwargs):
         """
         Updates matrix coefficients
@@ -358,6 +332,40 @@ class BasicLinearSystem(LinearSystem, ABC):
         mtx_dyn = np.diag(source_vector)
         self.mtx[:] = mtx_func.build_cell_conductance_matrix(
                 self.conductance) + mtx_dyn
+
+    def update_rhs(self, *args, **kwargs):
+        """
+        Create vector with the right hand side entries,
+        Sources from outside the src to the src must be defined negative.
+        """
+        self.rhs[:] = 0.0
+        rhs_input = kwargs.get('rhs_input', None)
+        if isinstance(rhs_input, RHSInput):
+            if isinstance(rhs_input.neumann_bc, BoundaryData):
+                self.set_neumann_boundary_conditions(
+                    rhs_input.neumann_bc.values,
+                    rhs_input.neumann_bc.axes,
+                    rhs_input.neumann_bc.indices)
+            if isinstance(rhs_input.dirichlet_bc, BoundaryData):
+                self.set_dirichlet_boundary_conditions(
+                    rhs_input.dirichlet_bc.values,
+                    rhs_input.dirichlet_bc.axes,
+                    rhs_input.dirichlet_bc.indices)
+            if isinstance(rhs_input.explicit_sources, SourceData):
+                sources = rhs_input.explicit_sources.values
+                source_vector = sources.ravel(order='F')
+                if rhs_input.dirichlet_bc is not None:
+                    index_vector_remaining = self.get_remaining_indices(
+                        rhs_input.dirichlet_bc.axes,
+                        rhs_input.dirichlet_bc.indices)
+                    source_vector[index_vector_remaining] = 0.0
+                self.add_source_data(
+                    source_vector,
+                    rhs_input.explicit_sources.indices,
+                    rhs_input.explicit_sources.volumetric
+                )
+        print('test')
+        # self.rhs[:] = self.rhs_const + self.rhs_dyn
 
     def get_remaining_indices(self, axes, indices):
         axes_patch = axes