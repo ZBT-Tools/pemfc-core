4e056349589532056c74f87ae23813d518bc176b
lufire <lukas.feierabend@gmail.com>
2024-10-15 13:42:57 +0200

    default not saving settings

--- pemfc/main_app.py
+++ pemfc/main_app.py
@@ -25,7 +25,7 @@ np.set_printoptions(threshold=sys.maxsize, linewidth=10000,
 np.seterr(all='raise')
 
 
-def main(settings=None, save_settings=True, print_iterations=False):
+def main(settings=None, save_settings=False, print_iterations=False):
     np.seterr(all='raise')
     start_time = timeit.default_timer()
     sim = simulation.Simulation(settings=settings)
