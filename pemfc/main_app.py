7b51c3dc806c8b5e84d985843754ddcf2433c231
lufire <lukas.feierabend@gmail.com>
2024-10-16 11:55:46 +0200

    stop making folder if nothing saved

--- pemfc/main_app.py
+++ pemfc/main_app.py
@@ -25,19 +25,15 @@ np.set_printoptions(threshold=sys.maxsize, linewidth=10000,
 np.seterr(all='raise')
 
 
-def main(settings=None, save_settings=False, print_iterations=False,
-         save_summary=False):
+def main(settings=None, print_iterations=False):
     np.seterr(all='raise')
     start_time = timeit.default_timer()
     sim = simulation.Simulation(settings=settings)
     sim.timing['start'] = start_time
     sim.timing['initialization'] = timeit.default_timer()
     # simulation.timing['start'] = start_time
-    g_data, l_data = sim.run(print_iterations=print_iterations,
-                             save_summary=save_summary)
+    g_data, l_data = sim.run(print_iterations=print_iterations)
     # sim.output.print_global_data(sim, g_data)
-    if save_settings:
-        sim.output.save_settings(sim.settings)
     return g_data, l_data, sim
 
 
