adc971a3c84224fdeeede596c425bd614621042a
lufire <lukas.feierabend@gmail.com>
2025-07-23 13:14:07 +0200

    relative imports

--- pemfc/src/fluid/evaporation_model.py
+++ pemfc/src/fluid/evaporation_model.py
@@ -5,8 +5,8 @@ import numpy as np
 from abc import ABC, abstractmethod
 from . import species
 from . import fluid as fl
-from pemfc.data import material_properties as mat_prop
-from pemfc.src import constants as constants
+from ...data import material_properties as mat_prop
+from ...src import constants as constants
 
 
 class EvaporationModel(ABC):
